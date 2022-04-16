757ec2ddddfecff0987d39888c9eb16ab2c4ed85
crazynavi <ilove.delicious.pizza@gmail.com>
2022-04-16 13:58:12 +0300

    Refactor Themed and StyledText

--- app/components/StyledText.tsx
+++ app/components/StyledText.tsx
@@ -1,5 +1,40 @@
-import { Text, TextProps } from './Themed';
+import { FONTS } from "app/constants/Assets";
+import { Text, TextProps } from "./Themed";
 
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
-}
+export const RegularText = (props: TextProps) => {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: FONTS.regular }]} />
+  );
+};
+
+export const LightText = (props: TextProps) => {
+  return <Text {...props} style={[props.style, { fontFamily: FONTS.light }]} />;
+};
+
+export const BoldText = (props: TextProps) => {
+  return <Text {...props} style={[props.style, { fontFamily: FONTS.bold }]} />;
+};
+
+export const MediumText = (props: TextProps) => {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: FONTS.medium }]} />
+  );
+};
+
+export const SemiText = (props: TextProps) => {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: FONTS.semibold }]} />
+  );
+};
+
+export const MonoText = (props: TextProps) => {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: FONTS.monoRegular }]} />
+  );
+};
+
+export const LightMonoText = (props: TextProps) => {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: FONTS.monoLight }]} />
+  );
+};
